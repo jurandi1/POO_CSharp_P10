2172c588e312c458176d6125f787bd48c63c4ad1
Jurandi Junior <jurandijr20@gmail.com>
2025-01-11 18:43:17 -0300

    Inferência de tipos: palavra var

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Inferência de tipos: palavra var");
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
         }
     }
 }
