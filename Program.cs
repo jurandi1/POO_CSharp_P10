34ddef04de4d5c2449295ab7bea25cf24bbdcca3
Jurandi Junior <jurandijr20@gmail.com>
2025-01-14 21:32:58 -0300

    DateTime

--- Program.cs
+++ Program.cs
@@ -9,6 +9,43 @@ namespace POO_CSharp_P10
             Console.WriteLine();
             Console.WriteLine("------------------------------------------------");
             Console.WriteLine();
+            Console.WriteLine("DateTime");
+            Console.WriteLine();
+
+            DateTime d1 = new DateTime(2000, 8, 15);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, 275);
+
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow;
+
+            DateTime d7 = DateTime.Parse("2000-08-05");
+            DateTime d8 = DateTime.Parse("2000-08-15 13:15:58");
+
+            DateTime d9 = DateTime.Parse("15/08/2000");
+            DateTime d10 = DateTime.Parse("05/08/2000 14:15:58");
+
+            DateTime d11 = DateTime.ParseExact("2000-08-05", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d12 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss",
+CultureInfo.InvariantCulture);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
+            Console.WriteLine(d12);
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine();
             Console.WriteLine("Funções interessantes para string");
 
             string original = "abcde FGHIJ ABC abc DEFG ";
